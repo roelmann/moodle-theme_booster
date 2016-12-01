08d289a9f2265f26ecc0299188a385a73483059f
roglos <roelmann@glos.ac.uk>
2016-12-01 09:07:09 +0000

    reduce lang file

--- lang/en/theme_booster.php
+++ lang/en/theme_booster.php
@@ -24,20 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['advancedsettings'] = 'Advanced settings';
-$string['brandcolor'] = 'Brand colour';
-$string['brandcolor_desc'] = 'The accent colour.';
 $string['choosereadme'] = 'booster is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
 $string['currentinparentheses'] = '(current)';
 $string['configtitle'] = 'booster';
-$string['generalsettings'] = 'General settings';
 $string['pluginname'] = 'booster';
-$string['presetfiles'] = 'Additional theme preset files';
-$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See https://docs.moodle.org/dev/booster_Presets for information on creating and sharing your own preset files.';
-$string['preset'] = 'Theme preset';
-$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
-$string['rawscss'] = 'Raw SCSS';
-$string['rawscss_desc'] = 'Use this field to provide SCSS code which will be injected at the end of the style sheet.';
-$string['rawscsspre'] = 'Raw initial SCSS';
-$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 $string['region-side-pre'] = 'Right';
